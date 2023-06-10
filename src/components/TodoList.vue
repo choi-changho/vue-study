14232c6cbfd523f246e5fbd0cd269d01a3fe9c06
choichangho <ghodt132@naver.com>
2023-06-10 17:11:59 +0900

    list check 로직 추가, 데이터 객체화

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,9 +1,14 @@
 <template>
   <div>
     <ul>
-      <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem" class="shadow">
-        {{ todoItem }}
-        <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+      <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem.item" class="shadow">
+        <span class="checkBtn" v-bind:class="{checkBtnCompleted: todoItem.completed}" v-on:click="toggleComplete(todoItem, index)">
+          <font-awesome-icon icon="check"/>
+        </span>
+        <span v-bind:class="{textCompleted: todoItem.completed}">
+          {{ todoItem.item }}
+        </span>
+        <span class="removeBtn" v-on:click="removeTodo(todoItem.item, index)">
           <font-awesome-icon icon="trash-can" />
         </span>
       </li>
@@ -24,13 +29,20 @@ export default {
       localStorage.removeItem(todoItem);
       // 현재 인스턴스 todoItems 배열 내 제거
       this.todoItems.splice(index,1);
+    },
+    toggleComplete: function (todoItem) {
+      todoItem.completed = !todoItem.completed;
+      // 로컬스토리지 데이터 갱신
+      localStorage.removeItem(todoItem.item);
+      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
     }
   },
   created: function () {
     if (localStorage.length > 0) {
       for (let i = 0; i < localStorage.length; i++) {
         if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
-          this.todoItems.push(localStorage.key(i));
+          var obj = JSON.parse(localStorage.getItem(localStorage.key(i)));
+          this.todoItems.push(obj);
         }
       }
     }
@@ -41,7 +53,7 @@ export default {
 <style scoped>
 ul {
   list-style-type: none;
-  padding-left: 0px;
+  padding-left: 0;
   margin-top: 0;
   text-align: left;
 }
@@ -55,6 +67,10 @@ li {
   background: white;
   border-radius: 5px;
 }
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
 .checkBtn {
   line-height: 45px;
   color: #63acde;
@@ -66,9 +82,5 @@ li {
 .textCompleted {
   text-decoration: line-through;
   color: #b3adad;
-}
-.removeBtn {
-  margin-left: auto;
-  color: #de4343;
 }
 </style>
