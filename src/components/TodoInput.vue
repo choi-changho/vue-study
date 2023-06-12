4d1f5b8b1e032d0c66ec9c0c415b38bec09bb1a0
choichangho <ghodt132@naver.com>
2023-06-12 21:10:22 +0900

    [컴포넌트 추가] Modal 컴포넌트 추가 1. slot 사용방법 2. transition 효과 3. v-on 디렉티브 @ 축소 사용

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -4,14 +4,25 @@
     <span class="addContainer" v-on:click="addTodo">
       <font-awesome-icon icon="plus" class="addBtn"/>
     </span>
+    <Modal v-if="showModal" @close="showModal = false">
+      <h3 slot="header">
+        경고!
+        <font-awesome-icon icon="circle-xmark" class="closeModalBtn" @click="showModal = false"/>
+      </h3>
+      <div slot="body">
+        아무것도 입력하지 않으셨습니다.
+      </div>
+    </Modal>
   </div>
 </template>
 
 <script>
+import Modal from './common/Modal.vue';
 export default {
   data: function () {
     return {
-      newTodoItem: ""
+      newTodoItem: "",
+      showModal: false
     }
   },
   methods: {
@@ -20,11 +31,16 @@ export default {
         // 상단에서 넘겨준 이벤트를 실행 => 상단 영역에서 매핑된 이벤트 실행과 함께 파라미터 전달
         this.$emit('addTodoItem', this.newTodoItem);
         this.clearInput();
+      } else {
+        this.showModal = !this.showModal;
       }
     },
     clearInput: function () {
       this.newTodoItem = "";
-    }
+    },
+  },
+  components: {
+    Modal,
   }
 }
 </script>
@@ -53,5 +69,8 @@ input:focus {
 .addBtn {
   color: white;
   vertical-align: middle;
+}
+.closeModalBtn {
+  color: #42b983;
 }
 </style>
